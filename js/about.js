ea953b905b195a682dc95669fac69b3fe18fe792
shivsaha1999 <shivangisaha.2806@gmail.com>
2024-04-04 11:50:58 +0530

    Remove unused files and clean up code

--- js/about.js
+++
@@ -1,3 +0,0 @@
-// This file is intentionally left blank.
-// Add your JavaScript code specific to the about section here.
-// Remember to link this file to the about.html file using a script tag in the HTML file.
--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-// This file is intentionally left blank.
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SERVICES MODAL ===============*/
+
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+
+
+/* Link active work */ 
+
+
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== LIGHT DARK THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
